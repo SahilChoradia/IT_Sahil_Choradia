aeb63f868628d5ff6ade928d4c6ef846d31cff70
SahilChoradia <121996546+SahilChoradia@users.noreply.github.com>
2023-01-05 09:45:21 +0530

    Add files via upload

---
+++ OOP.py
@@ -0,0 +1,13 @@
+class Employee:
+
+    def display():
+        n1=5
+        if (n1%2==0):
+           print("it is an evn number")
+        else:
+         print("It is not an odd number")
+        
+
+emp = Employee()
+emp.display()
+
